e0e33b1c5840df6458ab5d3747bc0e7c419b10bc
Niyati <niyati9194@gmail.com>
2024-06-12 23:52:44 -0400

    Update oracle_query_script.py
    Removed unnecessary modules

--- oracle-query-exporter/oracle_query_script.py
+++ oracle-query-exporter/oracle_query_script.py
@@ -1,12 +1,8 @@
 import cx_Oracle
 import pandas as pd
-import time
 import os
-from tkinter import Tk, Label, Entry, Button, filedialog, StringVar, OptionMenu, messagebox, DoubleVar, X
-from tkinter import ttk
+
 # Oracle database connection details
-# def execute_oracle_query(lib_dir, progress_var):
-print("start")
 oracle_host = 'host name'
 oracle_port = 'port number'
 oracle_sid = 'sid name'
@@ -18,36 +14,24 @@ shared_network_location = r'\\path\to\shared\location'
 
 # Oracle query to be executed
 oracle_query = """
-select SITE_NAME, LATITUDE,LONGITUDE,BAND_INFO,BAND_CLASS_NAME,TECHNOLOGY,PCI,PSS_ID,SSS_ID,PRACH_ROOT_SEQUENCES from SITES_ATOLL_V where ATOLL_SCHEMA in ('carolinas','nashville') AND SITE_VERSION in ('0000','0001')
+SELECT SITE_NAME, LATITUDE, LONGITUDE, BAND_INFO, BAND_CLASS_NAME, TECHNOLOGY, PCI, PSS_ID, SSS_ID, PRACH_ROOT_SEQUENCES FROM SITES_ATOLL_V WHERE ATOLL_SCHEMA IN ('carolinas', 'nashville') AND SITE_VERSION IN ('0000', '0001')
 """
 
 # Directory where Oracle Instant Client is installed.
 # Update this path based on your Oracle Instant Client installation.
 lib_dir = r"C:\ORACLE\instantclient_18_3"
 
-try:
-    cx_Oracle.init_oracle_client(lib_dir=lib_dir)
-    # print("success")
-except Exception as err:
-    # print("Error connecting: cx_Oracle.init_oracle_client()")
-    print(err)
-    # messagebox.showerror("Error",f"An error occurred: {str(err)}")
+# Initialize cx_Oracle client
+cx_Oracle.init_oracle_client(lib_dir=lib_dir)
 
-    
-
-
-    # Establish connection to Oracle database
-    # try:   
-dsn = cx_Oracle.makedsn(host=oracle_host, port=oracle_port , sid=oracle_sid )    
-
+# Establish connection to Oracle database
+dsn = cx_Oracle.makedsn(host=oracle_host, port=oracle_port, sid=oracle_sid)
 connection = cx_Oracle.connect(
     user=oracle_user,
     password=oracle_password,
-    # dsn=f'{oracle_host}:{oracle_port}/{oracle_sid}'
-    dsn= dsn
+    dsn=dsn
 )
 
-start_time = time.time()
 # Execute the query
 cursor = connection.cursor()
 cursor.execute(oracle_query)
@@ -56,53 +40,17 @@ cursor.execute(oracle_query)
 results = cursor.fetchall()
 
 # Close cursor and connection
-
+cursor.close()
+connection.close()
 
 # Convert results to pandas DataFrame
 df = pd.DataFrame(results, columns=[col[0] for col in cursor.description])
 
 # Save DataFrame to Excel file in shared network location
 excel_filename = 'oracle_query_results.xlsx'
-excel_file_path = f'{shared_network_location}/{excel_filename}'
+excel_file_path = os.path.join(shared_network_location, excel_filename)
 
 if os.path.exists(excel_file_path):
     os.remove(excel_file_path)
 
 df.to_excel(excel_file_path, index=False)
-
-# messagebox.showinfo(f"Query results saved to: {excel_file_path}")
-
-cursor.close()
-connection.close()
-    # except Exception as err:
-    #      messagebox.showerror("Error",f"An error occurred: {str(err)}")
-    # finally:
-    #      progress_var.set(100)
-
-# window = Tk()
-# window.title("Oracle Query Executor")
-
-# lib_dir_label = Label(window, text="Client Directory:")
-# lib_dir_label.pack(pady=(20,5))
-# lib_dir_entry = Entry(window, width = 70)
-
-# lib_dir_entry.pack(pady= 5)
-
-# progress_var = DoubleVar()
-# progress_bar = ttk.Progressbar(window, variable=progress_var, maximum=100)
-# progress_bar.pack(fill=X, padx=10,pady=5)
-
-# def execute_query():
-#      lib_dir = lib_dir_entry.get()
-#      progress_var.set(0)
-#      execute_oracle_query(lib_dir, progress_var)
-
-# button = Button(window, text="Execute Oracle Query", command= execute_query)
-# button.pack()
-
-# window.mainloop()
-         
-    # print("The execution time was..{}".format(time.time()-start_time))
-# print("done")
-
-
